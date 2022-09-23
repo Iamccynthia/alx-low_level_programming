57816ef483618c49a1a91861e92bdd04d242b8f0
Cynthia John <iammcynthiaa@gmail.com>
2022-09-22 23:29:15 -0200

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,11 +1,12 @@
 #include "main.h"
-/**
- *print_number - prints an integer.
- *only using the putchar function.
- *noarrays and pointers.
- *@n: integer to be printed.
- *Return: void.
+ /**
+ * print_number - prints an integer.
+ * only using the putchar function.
+ * noarrays and pointers.
+ * @n: integer to be printed.
+ * Return: void.
 */
+
 void print_number(int n)
 {
 
